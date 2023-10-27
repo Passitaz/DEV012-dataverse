bca57c615188747d2f8b2bd5ff2236a2517f8061
Passitaz <zpasillas.ma@gmail.com>
2023-10-27 12:51:04 -0600

    Función view

--- src/view.js
+++ src/view.js
@@ -1,6 +1,20 @@
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+
+  let lista = "";
+
+  data.forEach(element => {
+    lista += `<li>
+    <dl itemscope itemtype="WomenInTech">
+    <img class="imag" src="${element.imageUrl}" alt="Ada Lovelace" />
+    <dt>Nombre:</dt><dd itemprop="name">${element.name}</dd>
+    <dt>Descripción:</dt><dd itemprop="description">${element.shortDescription}</dd>
+    <dt>Año de nacimiento:</dt><dd itemprop="yearOfBirth">${element.facts.Genre}</dd>
+    <dt>Lugar de nacimiento:</dt><dd itemprop="placeOfBirth">London, England</dd>
+    <dt>Campo de desempeño:</dt><dd itemprop="mainField">Computer Science</dd>
+    </dl>
+    </li>`
+  });
+
+  return `<ul>${lista}</ul>`;
 };
 
