390bb793f7b5f951a497f1d92ccb07ac0ff254fc
Chío <claseschiounam@gmail.com>
2023-10-27 14:36:18 -0600

    cambios css

--- src/view.js
+++ src/view.js
@@ -2,19 +2,21 @@ export const renderItems = (data) => {
 
   let lista = "";
 
+  //lista.className= 'card';
   data.forEach(element => {
     lista += `<li>
-    <dl itemscope itemtype="WomenInTech">
-    <img class="imag" src="${element.imageUrl}" alt="Ada Lovelace" />
+  
+    <dl itemscope itemtype="Artist">"
+    <img class="imag" src="${element.imageUrl}" alt="imagen" />
     <dt>Nombre:</dt><dd itemprop="name">${element.name}</dd>
     <dt>Descripción:</dt><dd itemprop="description">${element.shortDescription}</dd>
-    <dt>Año de nacimiento:</dt><dd itemprop="yearOfBirth">${element.facts.Genre}</dd>
-    <dt>Lugar de nacimiento:</dt><dd itemprop="placeOfBirth">London, England</dd>
-    <dt>Campo de desempeño:</dt><dd itemprop="mainField">Computer Science</dd>
+    <dt>Género:</dt><dd itemprop="yearOfBirth">${element.facts.Genre}</dd>
+    <dt>Número de Albumnes:</dt><dd itemprop="placeOfBirth">${element.facts.Albums}</dd>
+    <dt>Solista o grupo:</dt><dd itemprop="mainField">${element.facts.Artist}</dd>
     </dl>
     </li>`
+    
   });
-
+ 
   return `<ul>${lista}</ul>`;
 };
-
