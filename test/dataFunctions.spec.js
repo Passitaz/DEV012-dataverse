a7b553d75cd7b736933298f88f918db1d2499db1
Passitaz <zpasillas.ma@gmail.com>
2023-11-09 14:21:25 -0600

    Primeras 2 pruebas unitarias correctas.2

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -6,18 +6,18 @@ import { data as fakeData } from "./data.js";
 describe('computeStats', () => {
   it('debe devolver el total correcto de álbumes', () => {
     // Llama a la función con un conjunto de datos de prueba
-    const mockData = [
-      { facts: { albums: 100 } },
-      { facts: { albums: 200 } },
-      { facts: { albums: 24 } },
-    ];
+    //const mockData = [
+    //  { facts: { albums: 100 } },
+    //  { facts: { albums: 200 } },
+    //  { facts: { albums: 24 } },
+    //];
 
     const resultado = computeStats(fakeData);
-    const resultado2 = computeStats(mockData);
+    //const resultado2 = computeStats(mockData);
     console.log(resultado);
 
     // Comprueba si el resultado es igual al valor esperado
-    expect(resultado).toBe(27);
+    expect(resultado).toBe(324);
     //expect(resultado2).toBe(324);
   });
 });
@@ -26,6 +26,6 @@ describe('filterData', () => {
   it ('Debe devolver el total de canciones de genero Rock', () => {
     const totalCancionesRock = filterData (fakeData, "genre", "Rock")
 
-    expect(totalCancionesRock.length).toBe(1);
+    expect(totalCancionesRock.length).toBe(8);
   });
 });
